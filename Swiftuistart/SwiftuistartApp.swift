3bfbed7bd04a252d2d8ca0beaa13e194509ec624
Shahzaibhey <shahzaibhere12@gmail.com>
2026-05-11 22:47:00 +0500

    Initial Commit

---
+++ Swiftuistart/SwiftuistartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuistartApp.swift
+//  Swiftuistart
+//
+//  Created by Shahzaib on 11/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuistartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
