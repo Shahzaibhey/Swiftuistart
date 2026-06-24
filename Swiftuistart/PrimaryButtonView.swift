1fb45dc85c01c0ed0e3481a47136321cd7d689bb
Shahzaibhey <shahzaibhere12@gmail.com>
2026-06-24 11:20:52 +0500

    Style the primary button

--- Swiftuistart/PrimaryButtonView.swift
+++ Swiftuistart/PrimaryButtonView.swift
@@ -1,5 +1,12 @@
 import SwiftUI
 
+/*
+ 1. background - Blue tint
+ 2. 24 corner radius
+ 3. button's title color - white
+ 4. push to the github
+ */
+
 struct PrimaryButtonView: View {
     
     @State private(set) var needsToShowSomeContent: Bool = false
@@ -12,12 +19,21 @@ struct PrimaryButtonView: View {
             } label: {
                 Label {
                     Text("Primary Button")
+                      
+                        
                 } icon: {
                     Image(systemName: "heart.fill")
                 }
+                .foregroundColor(.white)
+                .padding()
+                .background(.blue)
+                .cornerRadius(24)
+            
                 
             }
+            
         }
+        
         
         if needsToShowSomeContent {
             RoundedRectangle(cornerRadius: 24)
@@ -25,15 +41,20 @@ struct PrimaryButtonView: View {
         }
     }
     
+    
+    
+    
+    
     func handlePrimaryButtonAction() {
         withAnimation {
             needsToShowSomeContent.toggle()
         }
     }
+    
 }
-
-struct PrimaryButtonView_Previews: PreviewProvider {
-    static var previews: some View {
-        PrimaryButtonView()
+    struct PrimaryButtonView_Previews: PreviewProvider {
+        static var previews: some View {
+            PrimaryButtonView()
+        }
     }
-}
+
