b2e5205be0f203b2a775068179882ac0c2378937
Shahzaibhey <shahzaibhere12@gmail.com>
2026-06-24 11:22:53 +0500

    Refactor code

--- Swiftuistart/PrimaryButtonView.swift
+++ Swiftuistart/PrimaryButtonView.swift
@@ -1,12 +1,5 @@
 import SwiftUI
 
-/*
- 1. background - Blue tint
- 2. 24 corner radius
- 3. button's title color - white
- 4. push to the github
- */
-
 struct PrimaryButtonView: View {
     
     @State private(set) var needsToShowSomeContent: Bool = false
@@ -19,8 +12,6 @@ struct PrimaryButtonView: View {
             } label: {
                 Label {
                     Text("Primary Button")
-                      
-                        
                 } icon: {
                     Image(systemName: "heart.fill")
                 }
@@ -28,22 +19,14 @@ struct PrimaryButtonView: View {
                 .padding()
                 .background(.blue)
                 .cornerRadius(24)
-            
-                
             }
-            
         }
-        
         
         if needsToShowSomeContent {
             RoundedRectangle(cornerRadius: 24)
                 .frame(width: 240, height: 120)
         }
     }
-    
-    
-    
-    
     
     func handlePrimaryButtonAction() {
         withAnimation {
@@ -52,9 +35,10 @@ struct PrimaryButtonView: View {
     }
     
 }
-    struct PrimaryButtonView_Previews: PreviewProvider {
-        static var previews: some View {
-            PrimaryButtonView()
-        }
+
+struct PrimaryButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        PrimaryButtonView()
     }
+}
 
