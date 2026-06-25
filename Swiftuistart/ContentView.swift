ae9dbfddbe2deb51aae8bb44ac239076f6005acc
Shahzaibhey <shahzaibhere12@gmail.com>
2026-06-25 12:46:08 +0500

    Add Secondaryviewbutton with navigation flow

--- Swiftuistart/ContentView.swift
+++ Swiftuistart/ContentView.swift
@@ -4,20 +4,34 @@
 //
 //  Created by Shahzaib on 11/05/2026.
 //
-
 import SwiftUI
-
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20){
+            Image(systemName: "person.circle.fill")
+                .font(.system(size: 80))
+                .foregroundStyle(.blue)
+            Text("Shahzaib")
+                .font(.title)
+                .bold()
+            
+            Text("SwiftUI")
+                .font(.subheadline)
+            foregroundStyle(.gray)
+            
+            HStack(spacing: 20){
+                Label("Rawalpindi",systemImage: "location.fill")
+                Label("22year",systemImage: "calendar")
+                
+            }
+            .font(.caption)
+            foregroundStyle(.secondary)
         }
         .padding()
     }
 }
+
+
 
 #Preview {
     ContentView()
